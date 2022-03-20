18e8697637617a7e0627fcca1479d35b17350914
Yashvida Jayasekara <yashvida.1007@gmail.com>
2022-03-20 20:13:01 +0530

    travel and login changes

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -10,6 +10,7 @@ export class DashboardService {
 
   private currentStat = "https://www.hpb.health.gov.lk/api/get-current-statistical";
   private historyStat = "https://hpb.health.gov.lk/api/get-statistical-history-data";
+  private countryStat = "https://coronavirus-19-api.herokuapp.com/countries";
 
   constructor(private http: HttpClient) { }
 
@@ -19,5 +20,9 @@ export class DashboardService {
 
   getHistoricalStats = () =>{
     return this.http.get<HistoryStat>(this.historyStat);
+  }
+
+  getCountyStats = () =>{
+    return this.http.get<any>(this.countryStat);
   }
 }
